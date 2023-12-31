a2a3ae4ec549ba65f1ad60f77f6cbe0d57f8a3fb
t_kuroshita <t_kuroshita@uluru.com>
2023-12-31 16:29:58 +0900

    ✨ Navbar実装

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React from "react";
+// import icons
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import {
+  BsClipboard2Data,
+  BsBriefcase,
+  BsChatSquareText,
+} from "react-icons/bs";
+// link
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden x-50">
+      <div className="cantainer mx-auto">
+        {/* nav inner */}
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+          <Link
+            to="home"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BiHomeAlt />
+          </Link>
+          <Link
+            to="about"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BiUser />
+          </Link>
+          <Link
+            to="services"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsClipboard2Data />
+          </Link>
+          <Link
+            to="work"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            to="contact"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsChatSquareText />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
